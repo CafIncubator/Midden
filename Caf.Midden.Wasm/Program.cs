3ffcba650e58b210fb9931a42556def2a9355e06
Bryan Carlson <bryan.r.carlson@gmail.com>
2020-12-03 10:34:16 -0800

    Renamed Caf.Midden.Metadata to Caf.Midden.Wasm to avoid namespace conflicts, created state container

--- Caf.Midden.Wasm/Program.cs
+++ Caf.Midden.Wasm/Program.cs
@@ -8,7 +8,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Caf.Midden.Metadata
+namespace Caf.Midden.Wasm
 {
     public class Program
     {
---
+++ Caf.Midden.Wasm/Services/StateContainer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Caf.Midden.Core.Models.v0_1_0alpha4;
+using Microsoft.AspNetCore.Components;
+
+namespace Caf.Midden.Wasm.Services
+{
+    public class StateContainer
+    {
+        public Metadata Metadata { get; private set; }
+
+        public void SetMetadata(ComponentBase source, Metadata value)
+        {
+            this.Metadata = value;
+
+            NotifyStateChanged(source, "Metadata");
+        }
+
+        public event Action<ComponentBase, string> StateChanged;
+        private void NotifyStateChanged(
+            ComponentBase source,
+            string property) => StateChanged?.Invoke(source, property);
+    }
+}
