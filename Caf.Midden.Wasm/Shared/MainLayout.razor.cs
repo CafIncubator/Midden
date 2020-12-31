b10015711efc5f7d46f18764e5b76af2879d5ace
Bryan Carlson <bryan.r.carlson@gmail.com>
2020-12-30 16:05:38 -0800

    refined style and cleaned up code

---
+++ Caf.Midden.Wasm/Shared/MainLayout.razor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Caf.Midden.Wasm.Shared
+{
+    public partial class MainLayout
+    {
+        bool collapsed;
+        
+        void OnCollapse(bool isCollapsed)
+        {
+            // Nothing
+        }
+
+        void toggle()
+        {
+            collapsed = !collapsed;
+        }
+    }
+}
