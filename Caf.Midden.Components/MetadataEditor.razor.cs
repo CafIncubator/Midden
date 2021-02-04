ece47152a3cb4987c230e6f231f309b3032b9cff
Bryan Carlson <bryan.r.carlson@gmail.com>
2021-02-04 11:02:13 -0800

    Launch modal with new contact

--- Caf.Midden.Components/MetadataEditor.razor.cs
+++ Caf.Midden.Components/MetadataEditor.razor.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.Forms;
 using AntDesign;
 using Microsoft.AspNetCore.Components.Web;
+using Caf.Midden.Components.Modals;
 
 namespace Caf.Midden.Components
 {
@@ -97,17 +98,82 @@ namespace Caf.Midden.Components
             };
         }
 
+        private ModalRef personModalRef;
+        private async Task OpenPersonModalTemplate(Person contact)
+        {
+            var templateOptions = new ViewModels.PersonModalViewModel
+            {
+                Person = new Person()
+                {
+                    Name = contact.Name,
+                    Email = contact.Email,
+                    Role = contact.Role
+                },
+                Roles = AppConfig.Roles
+            };
+
+            var modalConfig = new ModalOptions();
+            modalConfig.Title = "Contact";
+            modalConfig.OnCancel = async (e) =>
+            {
+                await personModalRef.CloseAsync();
+            };
+            modalConfig.OnOk = async (e) =>
+            {
+                contact.Name = templateOptions.Person.Name;
+                contact.Email = templateOptions.Person.Email;
+                contact.Role = templateOptions.Person.Role;
+
+                await personModalRef.CloseAsync();
+            };
+
+            modalConfig.AfterClose = () =>
+            {
+                RemoveBlankContacts();
+
+                InvokeAsync(StateHasChanged);
+
+                return Task.CompletedTask;
+            };
+
+            personModalRef = await ModalService
+                .CreateModalAsync<PersonModal, ViewModels.PersonModalViewModel>(
+                    modalConfig, templateOptions);
+        }
+
+        private void RemoveBlankContacts()
+        {
+            List<Person> contactsToRemove = new List<Person>();
+            foreach(Person contact in this.Metadata.Dataset.Contacts)
+            {
+                if(string.IsNullOrWhiteSpace(contact.Name) &&
+                    string.IsNullOrWhiteSpace(contact.Email) &&
+                    string.IsNullOrWhiteSpace(contact.Role))
+                {
+                    contactsToRemove.Add(contact);
+                }
+            }
+            foreach(Person remove in contactsToRemove)
+            {
+                this.Metadata.Dataset.Contacts.Remove(remove);
+            }
+        }
         public void Dispose()
         {
             this.EditContext.OnFieldChanged -=
                 EditContext_OnFieldChange;
         }
 
-        private void AddContactHandler()
+        private async Task AddContactHandler()
         {
             if (this.Metadata.Dataset.Contacts == null)
                 this.Metadata.Dataset.Contacts = new List<Person>();
-            this.Metadata.Dataset.Contacts.Add(new Person());
+
+            var contact = new Person();
+
+            await OpenPersonModalTemplate(contact);
+
+            this.Metadata.Dataset.Contacts.Add(contact);
         }
 
         private void DeleteContactHandler(Person person)
