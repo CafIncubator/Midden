ee2636fdfe0f920bf177ed247e45617fe2589591
Bryan Carlson <bryan.r.carlson@gmail.com>
2021-02-04 16:27:23 -0800

    Added methods and derived works

---
+++ Caf.Midden.Components/ViewModels/MethodModalViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Caf.Midden.Core.Models.v0_1_0alpha4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caf.Midden.Components.ViewModels
+{
+    public class MethodModalViewModel
+    {
+        public string Method { get; set; }
+
+    }
+}
