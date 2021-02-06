825b382b0442e2e057541c39e1d9a8c68b53e984
Bryan Carlson <bryan.r.carlson@gmail.com>
2021-02-05 16:47:34 -0800

    Added variable to editor

---
+++ Caf.Midden.Components/ViewModels/VariableModalViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Caf.Midden.Core.Models.v0_1_0alpha4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caf.Midden.Components.ViewModels
+{
+    public class VariableModalViewModel
+    {
+        public Variable Variable { get; set; }
+        public List<string> Tags { get; set; }
+        public List<string> ProcessingLevels { get; set; }
+        public List<string> QCFlags { get; set; }
+        public IEnumerable<string> SelectedTags { get; set; }
+        public IEnumerable<string> SelectedQCApplied { get; set; }
+    }
+}
