3e7e2e1b61511afead210298dfab588e59d0ffd6
Bryan Carlson <bryan.r.carlson@gmail.com>
2021-02-18 15:59:11 -0800

    Updated to output catalog

--- Caf.Midden.Cli/Services/LocalFileSystemCrawler.cs
+++ Caf.Midden.Cli/Services/LocalFileSystemCrawler.cs
@@ -1,5 +1,6 @@
 ﻿using Caf.Midden.Cli.Common;
 using Caf.Midden.Core.Models.v0_1_0alpha4;
+using Caf.Midden.Core.Services.Metadata;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -18,7 +19,7 @@ namespace Caf.Midden.Cli.Services
                 throw new ArgumentNullException("Directory not specified");
 
             if(!Directory.Exists(rootDirectory))
-                throw new ArgumentNullException("Directory does nto exist");
+                throw new ArgumentNullException("Directory does not exist");
 
             this.rootDirectory = rootDirectory;
         }
@@ -32,9 +33,31 @@ namespace Caf.Midden.Cli.Services
             return files.ToList();
         }
 
+
         public List<Metadata> GetMetadatas()
         {
-            throw new NotImplementedException();
+            var files = GetFileNames();
+
+            List<Metadata> metadatas = new List<Metadata>();
+
+            MetadataParser parser =
+                new MetadataParser(
+                    new MetadataConverter());
+
+            foreach (var file in files)
+            {
+                string json = File.ReadAllText(file);
+
+                Metadata metadata = parser.Parse(json);
+
+                string relativePath = Path.GetRelativePath(this.rootDirectory, file);
+
+                metadata.Dataset.DatasetPath = relativePath.Replace(".midden", "");
+
+                metadatas.Add(metadata);
+            }
+
+            return metadatas;
         }
     }
 }
