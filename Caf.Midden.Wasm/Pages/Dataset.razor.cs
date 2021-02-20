9296cdaa4899ce12f35eb79e0e1e7d4654a0e4df
Bryan Carlson <bryan.r.carlson@gmail.com>
2021-02-19 16:27:33 -0800

    Added metadata specific page with full route, subtle changes to design

---
+++ Caf.Midden.Wasm/Pages/Dataset.razor.cs
@@ -0,0 +1,68 @@
+﻿using Caf.Midden.Core.Models.v0_1_0alpha4;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Caf.Midden.Wasm.Pages
+{
+    public partial class Dataset : IDisposable
+    {
+        [Parameter]
+        public string ZoneName { get; set; }
+
+        [Parameter]
+        public string ProjectName { get; set; }
+
+        [Parameter]
+        public string DatasetName { get; set; }
+
+        Metadata Metadata { get; set; }
+
+        protected override void OnInitialized()
+        {
+            State.StateChanged += async (source, property)
+                => await StateChanged(source, property);
+
+            if (State?.Catalog != null)
+                SetMetadata();
+        }
+
+        private async Task StateChanged(
+            ComponentBase source,
+            string property)
+        {
+            if (source != this)
+            {
+                if (property == "UpdateCatalog")
+                {
+                    SetMetadata();
+                }
+
+                await InvokeAsync(StateHasChanged);
+            }
+        }
+
+        private void SetMetadata()
+        {
+
+            if(State != null && State.Catalog != null && State.Catalog.Metadatas != null)
+            {
+                var metadata = State.Catalog.Metadatas.FirstOrDefault(m =>
+                    (m.Dataset.Zone == this.ZoneName) && 
+                    (m.Dataset.Project == this.ProjectName) && 
+                    (m.Dataset.Name == this.DatasetName));
+
+                if(metadata != null)
+                    this.Metadata = metadata;
+            }
+        }
+
+        public void Dispose()
+        {
+            State.StateChanged -= async (source, property)
+                => await StateChanged(source, property);
+        }
+    }
+}
