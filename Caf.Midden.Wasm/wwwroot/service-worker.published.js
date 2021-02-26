7959f4ce06323d0e9bbe8931e2fc559139431f5f
Bryan Carlson <bryan.r.carlson@gmail.com>
2021-02-25 16:50:31 -0800

    Added routes.json to ignore integrety check

--- Caf.Midden.Wasm/wwwroot/service-worker.published.js
+++ Caf.Midden.Wasm/wwwroot/service-worker.published.js
@@ -9,7 +9,7 @@ self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
 const cacheNamePrefix = 'offline-cache-';
 const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
 const offlineAssetsInclude = [ /\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/ ];
-const offlineAssetsExclude = [ /^service-worker\.js$/ ];
+const offlineAssetsExclude = [ /^service-worker\.js$/, /^routes\.json$/ ];
 
 async function onInstall(event) {
     console.info('Service worker: Install');
