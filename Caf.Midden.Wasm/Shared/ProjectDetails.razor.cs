8068645f828135c7e62f7744c27f60d115ab443f
bryanrcarlson <bryan.r.carlson@gmail.com>
2022-02-16 12:42:21 -0800

    Added project details

---
+++ Caf.Midden.Wasm/Shared/ProjectDetails.razor.cs
@@ -0,0 +1,61 @@
+﻿using AntDesign;
+using Caf.Midden.Core.Models.v0_1;
+using Caf.Midden.Wasm.Shared.Modals;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Caf.Midden.Wasm.Shared
+{
+    public partial class ProjectDetails : IDisposable
+    {
+        [Parameter]
+        public string ProjectName { get; set; }
+
+        public Project Project { get; set; }
+
+        protected override void OnInitialized()
+        {
+            State.StateChanged += async (source, property)
+                => await StateChanged(source, property);
+
+            if (State?.Catalog != null)
+            {
+                SetProject();
+            }
+        }
+
+        private async Task StateChanged(
+            ComponentBase source,
+            string property)
+        {
+            if (source != this)
+            {
+                if (property == "UpdateCatalog")
+                {
+                    SetProject();
+                }
+
+                await InvokeAsync(StateHasChanged);
+            }
+
+
+        }
+
+        private void SetProject()
+        {
+            Project = State.Catalog.Projects
+                .Where(p =>
+                    (String.IsNullOrEmpty(p.Name) || p.Name.ToLower() == this.ProjectName.ToLower()))
+                .FirstOrDefault();
+        }
+
+        public void Dispose()
+        {
+            State.StateChanged -= async (source, property)
+                => await StateChanged(source, property);
+        }
+    }
+}
