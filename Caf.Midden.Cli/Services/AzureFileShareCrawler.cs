04fc785c660a3772f4ecf07b4f386bbd510f2990
bryanrcarlson <bryan.r.carlson@gmail.com>
2022-02-23 10:00:22 -0800

    Implemented GetProjects

--- Caf.Midden.Cli/Services/AzureFileShareCrawler.cs
+++ Caf.Midden.Cli/Services/AzureFileShareCrawler.cs
@@ -13,7 +13,8 @@ namespace Caf.Midden.Cli.Services
 {
     public class AzureFileShareCrawler : ICrawl
     {
-        private const string FILE_EXTENSION = ".midden";
+        private const string MIDDEN_FILE_EXTENSION = ".midden";
+        private const string MIPPEN_FILE_EXTENSION = ".mippen";
 
         private readonly string uri;
         private readonly string path;
@@ -102,7 +103,7 @@ namespace Caf.Midden.Cli.Services
                         {
                             remaining.Enqueue(dir.GetSubdirectoryClient(item.Name));
                         }
-                        else if (item.Name.Contains(FILE_EXTENSION))
+                        else if (item.Name.Contains(MIDDEN_FILE_EXTENSION))
                         {
                             Console.WriteLine($"  In {dir.Uri.AbsolutePath} found {item.Name}");
 
@@ -122,7 +123,7 @@ namespace Caf.Midden.Cli.Services
                             // Sets the dataset path relative to the Uri and Path specified in constructor
                             var filePath = 
                                 Path.GetRelativePath(this.path, file.Path)
-                                .Replace(FILE_EXTENSION, "");
+                                .Replace(MIDDEN_FILE_EXTENSION, "");
 
                             metadata.Dataset.DatasetPath = filePath;
 
@@ -143,7 +144,53 @@ namespace Caf.Midden.Cli.Services
 
         public List<Project> GetProjects()
         {
-            throw new NotImplementedException();
+            List<Project> projects = new List<Project>();
+
+            try
+            {
+                var remaining = new Queue<ShareDirectoryClient>();
+
+                remaining.Enqueue(shareClient.GetDirectoryClient(this.path));
+                while (remaining.Count > 0)
+                {
+                    ShareDirectoryClient dir = remaining.Dequeue();
+                    foreach (ShareFileItem item in dir.GetFilesAndDirectories())
+                    {
+                        if (item.IsDirectory)
+                        {
+                            remaining.Enqueue(dir.GetSubdirectoryClient(item.Name));
+                        }
+                        else if (item.Name.Contains(MIPPEN_FILE_EXTENSION))
+                        {
+                            Console.WriteLine($"  In {dir.Uri.AbsolutePath} found {item.Name}");
+
+                            ShareFileClient file = dir.GetFileClient(item.Name);
+
+                            ShareFileDownloadInfo fileContents = file.Download();
+                            string fileString;
+                            using (MemoryStream ms = new MemoryStream())
+                            {
+                                fileContents.Content.CopyTo(ms);
+                                fileString = Encoding.UTF8.GetString(ms.ToArray());
+                            }
+
+                            Project project = new Project()
+                            {
+                                Name = file.Name.Replace(MIPPEN_FILE_EXTENSION, ""),
+                                Description = fileString
+                            };
+                        }
+                    }
+                }
+
+                Console.WriteLine($"Found a total of {projects.Count} files");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"An error ocurred: {e}");
+            }
+
+            return projects;
         }
     }
 }
